f8641112c4c99b1b036519219f71526d3c859550
bug-rancher <bug-rancher@protonmail.com>
2019-02-21 13:15:59 +0100

    fixed missed updates

--- src/depositions_text_injection/repair_wrong_xml_id.py
+++ src/depositions_text_injection/repair_wrong_xml_id.py
@@ -3,8 +3,11 @@ import os
 import sys
 
 def repair_ids(text: str) -> str:
-    text = re.sub('(<.*?xml:id=")([0-9].*?")', "\g<1>osf\g<2>", text) #  replace id's in tags with explixit xml:id value, if it starts with digit
-    text = re.sub('(<text.*?)#id([0-9].*?")', "\g<1>#idosf\g<2>", text) #  replace id's in text @corresp tags, if it starts with digit
+    # replace id's in tags with explixit xml:id value, if it starts with digit
+    text = re.sub('<([a-zA-Z_][A-Za-z-_.]*?) ([^<^>]?xml:id=")([0-9].*?")', "<\g<1> \g<2>\g<1>\g<3>", text)
+
+    # replace id's in text @corresp tags, if it starts with digit
+    text = re.sub('(<text.*?)#id([0-9].*?")', "\g<1>#p\g<2>", text)
     return text
 
 
